a71d1126343a2974eaf4926719fc01da8113c541
Tamara & Aleksandar <Tamara & Aleksandar@ALEKSANDAR>
2017-07-25 20:38:14 +0200

    Added most popular part in post page

---
+++ HippieTamTam/Controllers/BlogPostController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HippieTamTam.Controllers
+{
+    public class BlogPostController : Controller
+    {
+        // GET: BlogPost
+        public ActionResult Post()
+        {
+            return View();
+        }
+    }
+}
