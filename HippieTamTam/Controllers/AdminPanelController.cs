199661cb64900e388072a41baf8d8e9fbc8759f8
Tamara & Aleksandar <Tamara & Aleksandar@ALEKSANDAR>
2017-08-23 15:59:08 +0200

    working create function in admin panel

--- HippieTamTam/Controllers/AdminPanelController.cs
+++ HippieTamTam/Controllers/AdminPanelController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using HippieTamTam.Models;
+using System.IO;
 
 namespace HippieTamTam.Controllers
 {
@@ -62,21 +63,57 @@ namespace HippieTamTam.Controllers
         
         public ActionResult Create(FormCollection form)
         {
-
             int id = int.Parse(form["CreateDDL"]);
+
+            var lays = DbData.Layouts.ToList();
+            var cats = DbData.Categories.ToList();
+            var post = new Post
+            {
+                LayoutID = id
+            };
+            LaysCatsPost lcp = new LaysCatsPost();
+            lcp.Categories = cats;
+            lcp.Layouts = lays;
+            lcp.Post = post;
+
+            
+            
             if (id.GetType() == typeof(int) && id != 0)
             {
                 var layout = DbData.Layouts.Where(l => l.LayoutID == id).SingleOrDefault();
 
-                return View("Create", "~/Views/Shared/Afirmacije/" + layout.LayoutName + "c.cshtml");
+                return View("Create", "~/Views/Shared/Afirmacije/" + layout.LayoutName + "c.cshtml",lcp);
             }
             else
                 return RedirectToAction("Index", "AdminPanel");
         }
 
         
-        public ActionResult CreatePost(Post p)
+        public ActionResult CreatePost(LaysCatsPost lcp, HttpPostedFileBase uploadImage)
         {
+            string pic = Path.GetFileName(uploadImage.FileName);
+            string path = Path.Combine(System.Web.Hosting.HostingEnvironment.MapPath("~/Images"), pic);
+            uploadImage.SaveAs(path);
+
+            path = "../../Images/" + pic;
+
+            var post = new Post
+            {
+                PostTitle = lcp.Post.PostTitle,
+                PostBackground = path.ToString(),
+                PostBackgroundColor= lcp.Post.PostBackgroundColor,
+                PostSubhead1= lcp.Post.PostSubhead1,
+                PostText1= lcp.Post.PostText1,
+                PostText2= lcp.Post.PostText2,
+                PostQuote1= lcp.Post.PostQuote1,
+                CategoryID= lcp.Post.CategoryID,
+                LayoutID=lcp.Post.LayoutID,
+                PostDateCreated= DateTime.Now,
+                PostAuthor= lcp.Post.PostAuthor
+            };
+
+            DbData.Posts.Add(post);
+            DbData.SaveChanges();
             return RedirectToAction("Index", "AdminPanel");
         }
     }
---
+++ HippieTamTam/Models/LaysCatsPost.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HippieTamTam.Models
+{
+    public class LaysCatsPost
+    {
+        public List<Category> Categories { get; set; }
+        public List<Layout> Layouts { get; set; }
+        public Post Post { get; set; }
+    }
+}
